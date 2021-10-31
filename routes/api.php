c1e3d8bb042635504b3e418d0586acc50deb87c3
rizwanullah313 <ru323091@gmail.com>
2021-10-31 12:12:46 +0000

    working on get with routers

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::get('/users/{name}', function($name){
+//     return 'Hi '. $name;
+// });
+//http://localhost:8000/api/users/rizwan
+
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+// Route::get('/users/{name?}', function($name = null){
+//     return 'Hi '. $name;
+// });
+// jab ? ur null add kia thu per khali load hwa werna like uer jesa cdehy usmey inputdena zarory hy
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/users', function(){
-    return "Hi Users";
-});
+Route::get('/users/{name?}', function($name = null){
+    return 'Hi '. $name;
+})->where('name', '[a-zA-Z]+');
+
+Route::get('/products/{id?}', function($id = null){
+    return 'Product Id is '. $id;
+})->where('id', '[0-9]+');
